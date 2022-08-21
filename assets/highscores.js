0f50ec3c2ec6ab1fc1625dbf449a8dbc3b238844
sa1987 <s.aswathi.13@gmail.com>
2022-08-21 18:04:31 +0100

    Modified highscore format

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,8 +1,42 @@
-const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
+
+// highScoresList.innerHTML = highScores
+//   .map(score => {
+//     return `<li class="high-score">${score.name} - ${score.score}</li>`;
+//   })
+//   .join("");
+
+
+//   function displayMovies(highScores){
+//     let table = '<table>';
+//     table += '<tr><th>Name</th><th>Score</th></tr>';
+//     movies.map((movie, index) => {
+//         table = table + '<tr>',
+//         table = table + '<td>' + 'Name:' + `${score.name}` + '</td>',
+//         table = table + '<td>' + 'Name:' + `${score.score}` + '</td>'
+//      });  
+//      table += "</table>"
+//      document.getElementById("movies-list").innerHTML = table;
+//  }
+ 
+//  highScoresList.innerHTML = displayMovies(movies);
+
+
+ const displayHighscores = () => {
+  let table = '<table border="1">';
+  table += '<tr><th>ID</th><th>Score</th></tr>';
+  highScores.forEach((highScores) => {
+    table += `<tr>
+                <td>${highScores.name}</td>
+                <td>${highScores.score}</td>
+              </tr>`;
+  });
+//  console.log(table); 
+  table += '</table>';
+  document.getElementById('highScoresList').innerHTML = table;
+};
+
+displayHighscores();
+//highScoresList.innerHTML = displayMovies(highScores);
+ 
